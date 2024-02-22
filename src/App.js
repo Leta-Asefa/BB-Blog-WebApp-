2bab60242a287ec70db7ab60f7a97c5627c58ece
Leta Asefa <letaassefa7@gmail.com>
2024-02-22 14:23:04 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import FoodItems from './Components/FoodItems'
 import { useState } from 'react';
 import ToDoList from './Components/ToDoList';
+import UseEffect from './Components/UseEffect';
+
 
 function App() {
 
 
 
   return (
-    <ToDoList />
-
+    
 
   );
 
